4be4b38690684d0ef1caa2ecaca07e3c7e1ac885
Fabiano <fabiano.ssantos79@gmail.com>
2024-02-05 01:30:12 -0300

    Tarefa 6 - Aplicando restrição, Getters e Setters na classe Produto

--- src/main/java/br/com/alura/comex/semana4/Produto.java
+++ src/main/java/br/com/alura/comex/semana4/Produto.java
@@ -1,14 +1,42 @@
 package br.com.alura.comex.semana4;
 
 public class Produto {
-    String nome;
-    String descricao;
-    double precoUnitario;
-    int quantidade;
+    private String nome;
+    private String descricao;
+    private double precoUnitario;
+    private int quantidade;
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public double getPrecoUnitario() {
+        return precoUnitario;
+    }
+    public void setPrecoUnitario(double precoUnitario) {
+        this.precoUnitario = precoUnitario;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
 
     public void imprimeDados(){
         System.out.println(">>> Dados do produto");
-        System.out.println("Nome: " + nome);
-        System.out.println("Descrição: "+ descricao);
+        System.out.println("Nome: " + this.nome);
+        System.out.println("Descrição: "+ this.descricao);
     }
 }
