eb0cd6764239946dd024dde68a7f280a4b93a82e
Fabiano <fabiano.ssantos79@gmail.com>
2024-02-14 00:45:09 -0300

    Tarefa 8 - Json para Objeto

---
+++ src/main/java/br/com/alura/comex/semana5/APIConverter.java
@@ -0,0 +1,4 @@
+package br.com.alura.comex.semana5;
+
+public record APIConverter(int id, String title, double price, String description, String category, String image) {
+}
--- src/main/java/br/com/alura/comex/semana5/PrincipalApi.java
+++ src/main/java/br/com/alura/comex/semana5/PrincipalApi.java
@@ -1,6 +1,8 @@
 package br.com.alura.comex.semana5;
 
+import com.google.gson.FieldNamingPolicy;
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 import java.io.IOException;
 import java.net.URI;
@@ -12,19 +14,31 @@ import java.util.Scanner;
 public class PrincipalApi {
     public static void main(String[] args) throws IOException, InterruptedException {
 
-        int id;
         Scanner scanner = new Scanner(System.in);
         System.out.println("Digite o número do produto");
-        id = scanner.nextInt();
+        int id = scanner.nextInt();
+
+        Gson gson = new GsonBuilder()
+                .setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE)
+                .setPrettyPrinting()
+                .create();
+
+
         String endereco = "https://fakestoreapi.com/products/"+id;
 
+        try{
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder().uri(URI.create(endereco)).build();
-        HttpResponse<String>response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
         String json = response.body();
 
         System.out.println(json);
 
-        Gson gson = new Gson();
+        APIConverter minhaApiConvertida = gson.fromJson(json, APIConverter.class);
+        ProductApi myproductApi = new ProductApi(minhaApiConvertida);
+        System.out.println(myproductApi);
+        }catch(NumberFormatException erro){
+            System.out.println("Aconteceu um erro: " + erro.getMessage());
+        }
     }
 }
